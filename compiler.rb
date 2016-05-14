1458ac8fe6c1fe2bb741233cb8734bc9181dffe8
Lito Nicolai <lito.nicolai@gmail.com>
2016-05-14 15:44:52 -0700

    EBNF parser, missing some pieces

--- compiler.rb
+++ compiler.rb
@@ -48,11 +48,11 @@ def proc_a
   end
 end
 
-# Let's emulate the Oberon language Texts.Read!
-#
+# Let's emulate the Oberon language 'Texts' module!
+
 # An 'input' is similar to a file reader: it keeps track of the current
 # reading location in the string.
-class Input
+class Reader
   attr_reader :text
   attr_accessor :ptr
   def initialize text
@@ -66,10 +66,49 @@ class Input
   end
 end
 
+class Writer
+  attr_reader :buf
+
+  def initialize
+    @buf = ""
+  end
+
+  def concat str
+    buf << str
+  end
+end
+
 class Texts
-  def self.Read input, ch
-    ch[0] = input.text[input.ptr] # Mutuate ch in place
-    input.ptr += 1
+  # Oberon is pretty procedural, so instead of putting implementations on
+  # the classes themselves ('writer' and 'reader'), they are on this
+  # 'Texts' module.
+
+  # Reader procedures
+  def self.Read reader, ch
+    ch[0] = reader.text[reader.ptr] # Mutuate ch in place
+    reader.ptr += 1
+  end
+
+  def self.pos reader
+    reader.ptr
+  end
+
+  # Writer procedures
+
+  def self.WriteString writer, str
+    writer.concat str
+  end
+
+  def self.WriteInt writer, i, num_digits
+    writer.concat i.to_s[0...num_digits]
+  end
+
+  def self.WriteLn writer
+    writer.concat "\n"
+  end
+
+  def Append log, str
+    log << str
   end
 end
 
@@ -82,7 +121,7 @@ class OberonScanner
   attr_accessor :sym, :id
 
   def initialize program_text
-    @r = Input.new program_text
+    @r = Reader.new program_text
     @ch = ""
     @sym = ""
     @id = []
@@ -151,3 +190,113 @@ scanner = OberonScanner.new "hello world\0"
 scanner.GetSym
 error unless 0 == scanner.sym
 error unless "hello" == scanner.id.join
+
+
+LOG = ""
+
+class EBNFParser
+  attr_accessor :lastpos, :w
+
+  def initialize input
+    @scanner = OberonScanner.new input
+    @w = Writer.new
+    @lastpos = 0
+  end
+
+  def getSym
+    scanner.GetSym
+  end
+
+  def id
+    scanner.id
+  end
+
+  def sym
+    scanner.sym
+  end
+
+  def error errno
+    pos = Texts.pos(R)
+    if pos > lastpos + 4 # Not *too* many errors
+      Texts.WriteString(w, " pos"); Texts.WriteInt(w, pos, 6)
+      Texts.WriteString(w, " err"); Texts.WriteInt(w, errno, 4); @lastpos = pos
+      Texts.WriteString(w, " sym"); Texts.WriteInt(w, sym, 4)
+      Texts.WriteLn(w); Texts.Append(LOG, w.buf)
+    end
+  end
+
+  def record t, id, i
+    # I have *no* idea what this is supposed to do. It's omitted from
+    # the textbook code in one edition, and an empty proc in another.
+    # Maybe appends the literal to a list of literals?
+  end
+
+  def production
+    # A 'production rule'. Has the form
+    # ident = expr
+    getSym
+    if sym == EQL then getSym else error 7  end
+    expression
+    if sym == PERIOD then getSym else error 8 end
+  end
+
+  def expression
+    # One or more 'term's, separated by '|'
+    term
+    while sym == BAR do
+      factor
+    end
+  end
+
+  def term
+    # One or more factors
+    factor
+    while sym < bar do
+      factor
+    end
+  end
+
+  def factor
+    # factor = identifier
+    #        | string
+    #        | "(" expression ")"
+    #        | "[" expression "]"
+    #        | "{" expression "}"
+    if sym == IDENT
+      record(t0, id, 1)
+      getSym
+    elsif sym == LITERAL
+      record(t1, id, 0)
+      getSym
+    elsif sym == LPAREN
+      getSym
+      expression
+      if sym == RPAREN then getSym else error 2 end
+    elsif sym == LBRAK
+      getSym
+      expression
+      if sym == RBRAK then getSym else error 3 end
+    elsif sym == LBRACE
+      getSym
+      expression
+      if sym == RBRACE then getSym else error 4 end
+    else
+      error 5
+    end
+  end
+
+  def syntax
+    # Zero or more 'productions'
+    while sym == IDENT do
+      production
+    end
+  end
+
+  def compile
+    @lastpos = 0
+    Texts.Read(r, ch)
+    getSym
+    syntax
+    Texts.Append(LOG, W.buf)
+  end
+end
